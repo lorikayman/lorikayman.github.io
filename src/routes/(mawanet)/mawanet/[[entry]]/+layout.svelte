4c0db1a79a1b3bc038ee871465d29b0e91a528a2
ironkayman <kostya.orlik@outlook.com>
2024-01-04 01:24:10 +0400

    sidebar flexbox partial fix

--- src/routes/(mawanet)/mawanet/[[entry]]/+layout.svelte
+++ src/routes/(mawanet)/mawanet/[[entry]]/+layout.svelte
@@ -1,10 +1,6 @@
 <script>
   import "@fontsource/open-sans"
   import "./layout.css"
-  import Sidebar from "$lib/components/sidebar.svelte"
 </script>
 
-<Sidebar/>
-<div class="container">
-  <slot/>
-</div>
+<slot/>
