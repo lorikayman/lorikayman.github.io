e80418ab80442a628e60a63df9a91f882996689c
ironkayman <kostya.orlik@outlook.com>
2024-01-04 22:49:52 +0400

    Fixed wiki empty mdx loader

--- src/lib/stores/mawanet.loader.js
+++ src/lib/stores/mawanet.loader.js
@@ -1,6 +1,17 @@
 
+import { derived, writable } from "svelte/store";
+
 import Error404 from "$lib/components/error.404.svelte"
 import Error403 from "$lib/components/error.403.svelte"
+
+export const nextPageName = writable();
+export const currentPageName = writable();
+export const doesPageDiffer = derived(
+  [currentPageName, nextPageName],
+  ($values, set) => {
+    set($values[0] !== $values[1])
+  }
+)
 
 /**
  * Dynamically import an existing mdx componet (bundled)
@@ -12,10 +23,6 @@ export async function fetchComponent (componentName) {
     let error = undefined;
     try {
       switch (componentName) {
-        // case 'index':
-        //   EntryComponent = (await import('$lib/mawanet/readme.mdx')).default;
-        //   break;
-
         default:
           EntryComponent = Error403;
           break;
@@ -25,6 +32,7 @@ export async function fetchComponent (componentName) {
       EntryComponent = Error404;
       error = e;
     }
+    nextPageName.set(componentName.replace(/\_/g," ").split(' ')[0].toUpperCase());
     return {
         componentName,
         entryComponent: EntryComponent,
