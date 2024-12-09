010197eefa07b6b8792b679c3bc0b3195aeceeee
Ironkayman <lori.j.kayman@gmail.com>
2024-12-09 15:35:24 +0400

    TOC draft

--- src/routes/(mawanet)/mawanet/[[entry]]/+page.svelte
+++ src/routes/(mawanet)/mawanet/[[entry]]/+page.svelte
@@ -1,10 +1,40 @@
 <script>
-  export let data;
+  
+  import { createTableOfContents } from '@melt-ui/svelte';
+
   import { currentPageName } from "$lib/stores/mawanet.loader";
+  import Tree from '$lib/components/toc.svelte';
+  
+  export let data;
 
   if (data.error) console.error(data.error);
 
   $: updateTitle(data.pageName);
+ 
+  /**
+   * crete table of content by scanning a component
+   */
+  const {
+    elements: { item },
+    states: { activeHeadingIdxs, headingsTree },
+  } = createTableOfContents({
+    selector: '#toc-builder-preview',
+    exclude: ['h1',],
+    activeType: 'all',
+    headingFilterFn: (heading) => !heading.hasAttribute('data-toc-ignore'),
+    scrollFn: (id) => {
+      const container = document.getElementById('toc-builder-preview');
+      const element = document.getElementById(id);
+ 
+      if (container && element) {
+        container.scrollTo({
+          top: element.offsetTop - container.offsetTop - 16,
+          behavior: 'smooth',
+        });
+      }
+    },
+  });
+ 
 
   /**
    * Updates documents title reactively
@@ -19,4 +49,31 @@
   $: currentPageName.set(data.pageName.replace(/\_/g," ").split(' ')[0].toUpperCase());
 </script>
 
-<svelte:component this={data.pageComponent}></svelte:component>
+<div id="toc-builder-preview">
+  <svelte:component this={data.pageComponent}></svelte:component>
+</div>
+
+<div id="table-of-contents">
+  <nav>
+    {#key $headingsTree}
+      <Tree
+        tree={$headingsTree}
+        activeHeadingIdxs={$activeHeadingIdxs}
+        {item}
+      />
+    {/key}
+  </nav>
+</div>
+
+<style>
+
+@media (min-width: 890px) {
+  #table-of-contents {
+    position: fixed;
+    top: 5rem;
+    right: 1rem;
+    max-width: 10rem;
+  }
+}
+
+</style>
