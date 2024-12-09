e356babe5373cbea62a7ef045196941d6df39110
Ironkayman <lori.j.kayman@gmail.com>
2024-12-09 15:35:22 +0400

    Reworked preview image cropping-scaling logic

--- index.js
+++ index.js
@@ -13,15 +13,19 @@ for (let contactCard of document.querySelectorAll('.card-row')) {
         if (attrMap.hasAttribute('github')) {
             cv.style.backgroundImage = contactStyleParity.github;
             cv.style.backgroundPositionY = '0px';
+            cv.style.backgroundPositionX = '-20px';
         } else if (attrMap.hasAttribute('email')) {
             cv.style.backgroundImage = contactStyleParity.email;
             cv.style.backgroundPositionY = '-84px';
+            cv.style.backgroundPositionX = '0px';
         } else if (attrMap.hasAttribute('twitter')) {
             cv.style.backgroundImage = contactStyleParity.twitter;
             cv.style.backgroundPositionY = '0px';
+            cv.style.backgroundPositionX = '0px';
         } else if (attrMap.hasAttribute('discord')) {
             cv.style.backgroundImage = contactStyleParity.discord;
             cv.style.backgroundPositionY = '0px';
+            cv.style.backgroundPositionX = '0px';
         }
     })
 };
