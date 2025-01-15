9c1513b5cee927c7d13cd903e52646ad7d3056b1
Ironkayman <lori.j.kayman@gmail.com>
2025-01-16 02:28:33 +0400

    Stylized heading

---
+++ src/lib/entries/sk/components/sub_title_centered.svelte
@@ -0,0 +1,14 @@
+<div class="centered">
+    <slot />
+</div>
+
+<style>
+
+.centered {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    filter: drop-shadow(0 0 3mm hsla(39, 100%, 66%, 0.7));
+}
+
+</style>
