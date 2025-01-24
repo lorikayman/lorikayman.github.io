536741fee5df1a892fe8779bda20e2d39e723f5f
Ironkayman <lori.j.kayman@gmail.com>
2025-01-24 05:39:43 +0400

    Sync with sk-rebis

--- src/lib/entries/sk/components/sub_title_centered.svelte
+++ src/lib/entries/sk/components/sub_title_centered.svelte
@@ -4,11 +4,28 @@
 
 <style>
 
+@font-face {
+  font-family: "Open Sans Bold", sans-serif;
+  src: url("/static/fonts/Open_Sans/static/OpenSans-Bold.ttf");
+}
+
 .centered {
     display: flex;
     align-items: center;
     justify-content: center;
     filter: drop-shadow(0 0 3mm hsla(39, 100%, 66%, 0.7));
+
+    & code {
+        background-color: hsla(39, 100%, 66%, 1);
+        color: hsl(343, 10%, 10%);
+        padding: 4px 26px;
+        font-style: italic;
+        font-family: 'Open Sans Bold', sans-serif;
+    }
+
+    & h4 {
+        margin-bottom: 2rem
+    }
 }
 
 </style>
