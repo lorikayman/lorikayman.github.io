9c7a82fbb60018c6262e6e9c2791aec74e2f90b2
Ironkayman <lori.j.kayman@gmail.com>
2025-02-02 00:44:40 +0400

    Vite config cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-// import { mdsvex } from "mdsvex";
-// import { fileURLToPath } from 'node:url';
-//
 // import glsl from "vite-plugin-glsl";
-
-// import dynamicImportVariables from '@rollup/plugin-dynamic-import-vars';
 
 export default defineConfig({
   plugins: [sveltekit()],
