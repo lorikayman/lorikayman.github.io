df33fbb7d9397a6174f61713f4517f4dbb781490
Ironkayman <lori.j.kayman@gmail.com>
2025-02-02 00:43:18 +0400

    Hidden glsl import

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from "vite";
 // import { mdsvex } from "mdsvex";
 // import { fileURLToPath } from 'node:url';
 //
-import glsl from "vite-plugin-glsl";
+// import glsl from "vite-plugin-glsl";
 
 // import dynamicImportVariables from '@rollup/plugin-dynamic-import-vars';
 
 export default defineConfig({
-  plugins: [sveltekit(), glsl()],
+  plugins: [sveltekit()],
 });
