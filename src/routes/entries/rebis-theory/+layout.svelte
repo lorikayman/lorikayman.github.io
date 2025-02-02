13f12521d018b1718d5a80c7c117c3b6b387a607
Ironkayman <lori.j.kayman@gmail.com>
2025-02-02 05:52:08 +0400

    Partially fixed sk bg difference

--- src/routes/entries/rebis-theory/+layout.svelte
+++ src/routes/entries/rebis-theory/+layout.svelte
@@ -5,6 +5,7 @@
   // import Webgl from "$lib/components/canvas/webgl.svelte";
 </script>
 
+<div class="background-base"></div>
 <div class="background"></div>
 <!-- <Webgl controlClasses="background-special" /> -->
 <div>
