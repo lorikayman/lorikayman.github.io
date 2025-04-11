7cb25e061c1686a7a75f7223b211ed189f2e5fbc
Ironkayman <lori.j.kayman@gmail.com>
2025-04-11 05:22:29 +0400

    jsdoc for layout architecture

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -4,8 +4,10 @@ import { fileURLToPath } from "node:url";
 import { defineMDSveXConfig as defineConfig } from "mdsvex"
 // import { wikiLinkPlugin } from "remark-wiki-link";
 
-const dirname = path.resolve(fileURLToPath(import.meta.url), '..');
-const layoutPath = path.join(dirname, "./src/lib/entries/sk/mdsvex.layout.svelte");
+const root_dir = path.resolve(fileURLToPath(import.meta.url), '..');
+
+// mdsvex layouts' paths
+const layout_entries_sk_path = path.join(root_dir, "./src/lib/entries/sk/mdsvex.layout.svelte");
 
 const config = defineConfig({
   extensions: [
@@ -17,8 +19,23 @@ const config = defineConfig({
     dashes: true,
     backticks: false,
   },
+  /**
+   * This is one of more obscrure approaches
+   * of replacing html tags with custom svelte componets
+   * declared in a 'layout', -
+   * documented extermely poorly in mdsvex docs
+   *
+   * @see {@link https://www.youtube.com/watch?v=VJFkyGd0FEA}
+   * this video is a good enough demo of entire system
+   * of mdsvex layouts working as intended in a proper context.
+   *
+   * this is so bad
+   *
+   * @see {@link https://github.com/lorikayman/lorikayman.github.io/issues/8}
+   * for complete exploration of this feature
+   */
   layout: {
-    entries_sk: layoutPath,
+    entries_sk: layout_entries_sk_path,
   },
 });
 
--- src/lib/entries/sk/mdsvex.layout.svelte
+++ src/lib/entries/sk/mdsvex.layout.svelte
@@ -1,7 +1,4 @@
 <script context="module">
-  /**
-   * @see {@link https://www.youtube.com/watch?v=VJFkyGd0FEA}
-   */
   import h2 from "$lib/entries/sk/components/h2.svelte";
 
   export { h2 };
