5b8b90ad2e6f74af0ff230ad9cc7a514d46af0f5
Ironkayman <lori.j.kayman@gmail.com>
2025-04-11 19:57:05 +0400

    Notes to layouts

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -20,16 +20,28 @@ const config = defineConfig({
     backticks: false,
   },
   /**
-   * This is one of more obscrure approaches
-   * of replacing html tags with custom svelte componets
-   * declared in a 'layout', -
-   * documented extermely poorly in mdsvex docs
+   * Js developers reinventing and renaming 20+ year-old concepts at its fucking best
+   *
+   * This is one of more obscure but only approaches
+   * of replacing html tags with custom svelte components
+   * for MDX documents.
+   *
+   * We register here a mapping of layout names to their paths
+   * `_` name is the default layout.
+   * Those layouts are not the same as svelte layouts,
+   * as the prior is a concept restricted to specifically mdsvex
+   * and is the only feasible/programmable way
+   * to pacakge component subtitution through 2-step process:
+   *
+   * 1. We register an aforementioned mapping of layout name to its path
+   * 2. When we import an `.mdx` document, at its header we can specify
+   *    a layout by its name.
+   *    The specified layout can contain components,
+   *    exported by desired HTML tag names.
    *
    * @see {@link https://www.youtube.com/watch?v=VJFkyGd0FEA}
    * this video is a good enough demo of entire system
    * of mdsvex layouts working as intended in a proper context.
-   *
-   * this is so bad
    *
    * @see {@link https://github.com/lorikayman/lorikayman.github.io/issues/8}
    * for complete exploration of this feature
