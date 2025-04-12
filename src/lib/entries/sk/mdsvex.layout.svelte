a00822dee5393fc312bcb4c7618297f227bab107
Ironkayman <lori.j.kayman@gmail.com>
2025-04-13 02:47:47 +0400

    Cleanup

--- src/lib/entries/sk/mdsvex.layout.svelte
+++ src/lib/entries/sk/mdsvex.layout.svelte
@@ -1,8 +1,8 @@
 <script context="module">
-  // import h2 from "$lib/entries/sk/components/h2.svelte";
-  import a from "$lib/entries/sk/components/a.svelte";
+  import a from "$lib/entries/sk/components/a.svelte"
+  // import { h2, h3, h4, h5, h6 } from "$lib/entries/sk/components/h.svelte"
 
-  export { a };
+  // export {a, h2, h3, h4, h5, h6, };
 </script>
 
 <slot />
