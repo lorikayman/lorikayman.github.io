770dde171bfdf84523705e4006de85ab89f01d42
Lori Kayman <lori.j.kayman@gmail.com>
2025-05-17 01:22:08 +0400

    jsdoc cleanup

--- src/lib/events/toc.js
+++ src/lib/events/toc.js
@@ -3,13 +3,16 @@
  *
  * Table of contents lifecycle events
  * @see src/lib/components/toc.svelte
+ *
+ * TODO: migrate to factory functions with unique
+ * identifiers of events per component instances,
+ * tho ToC won't be as such
  */
 
 /**
- *
+ * ToC component DOM structure completion
  *
  * It should be fired once per build completion of a toc component
- * @see
  */
 export const EVENT_TOC_BUILD_COMPLETE = new CustomEvent(
   'toc:build:complete',
